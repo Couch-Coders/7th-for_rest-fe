e4b251c8a7334e770cdad9a555bea310a49dc374
jun-05 <58911113+jun-05@users.noreply.github.com>
2022-04-16 19:59:58 +0900

    Update MenuContainer.jsx

--- src/containers/main/MenuContainer.jsx
+++ src/containers/main/MenuContainer.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import ThumbnailTemplate from '../../components/main/humbnail/ThumbnailTemplate';
+import ThumbnailTemplate from '../../components/main/thumbnail/ThumbnailTemplate';
 import { useState } from 'react';
 import CatTagModal from './../../components/main/modal/CatTagModal';
 import RegionTagModal from '../../components/main/modal/RegionTagModal';
